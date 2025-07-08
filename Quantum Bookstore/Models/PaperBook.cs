3759ea6189fde7881a0e214416bf18cbfb30496c
omarovici <omarkhalidsaber042004@gmail.com>
2025-07-08 18:08:16 +0300

    Add PaperBook Class

---
+++ Quantum Bookstore/Models/PaperBook.cs
@@ -0,0 +1,13 @@
+namespace Quantum_Bookstore.Models;
+
+public class PaperBook : Book
+{
+    public int Stock {get; set;}
+    
+    public PaperBook(string title, string author, string publisher, int year, int price, int stock)  
+        : base(title, author, publisher, year, price)
+    {
+        if(stock<0) throw new Exception("Stock cannot be negative");
+        Stock = stock;
+    }
+}
